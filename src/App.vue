c13c50c4afc21c4bf524dde5f8ae99f98f530855
Nikita <nekit.zernov@yandex.ru>
2022-07-02 18:40:40 +0300

    refactoring, create full structure for 1 elevator

--- src/App.vue
+++ src/App.vue
@@ -1,82 +1,151 @@
 <template>
-<div class="main">
-  <div v-for="(num,index) in (floorCount+1)" :key="index">
-    <div :style="{top:`${(index*100) +5}px`}" class="grid">
+  <main class="main">
+    <div v-for="(num, index) in floorCount + 1" :key="index">
+      <div :style="{ top: `${index * 100 + 5}px` }" class="grid"></div>
     </div>
-  </div>
-  <div class="floors" ref="floors" :style="{height:`${this.floorCount*100}px`}">
-    <div class="elevator" ref="elevator" >
+    <div
+      class="floors"
+      ref="floors"
+      :style="{ height: `${this.floorCount * 100}px` }"
+    >
+      <div 
+        class="elevator" 
+        :class="{blink: isBlink}" 
+        ref="elevator"
+      ></div>
     </div>
-  </div>
-  <div v-for="(num,index) in floorCount" :key="index">
-    <button :style="{top:`${(index*100) +45}px`}" class="elevator-button" @click="btnhandler(num)">
-    </button>
-  </div>
-</div>
+    <div v-for="(num, index) in floorCount" :key="index">
+      <div
+        :style="{ top: `${index * 100 + 45}px` }"
+        class="elevator-button"
+        :class="{active: isActive.includes(num)}"
+        @click="btnHandler(num, true)"
+      ></div>
+    </div>
+  </main>
 </template>
 
 <script>
 // import ElevatorMain from './components/ElevatorMain.vue'
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     // ElevatorMain
   },
+  watch: {
+    isBusy(){
+      if (this.isActive.length && !this.isBusy){
+        this.btnHandler(this.isActive[0], false)
+      }
+    }
+  },
   data() {
     return {
-      floorCount:7,
-      curentFloor: 0
-    }
+      floorCount: 6,
+      curentFloor: 0,
+      isActive: [],
+      isBlink:null,
+      isBusy: false
+    };
+  },
+  mounted() {
+    this.$refs.elevator.style.top = `${(this.floorCount-1) * 100}px`
   },
   methods: {
-    btnhandler(floorNumber){
-      console.log(Math.abs(this.curentFloor - (this.floorCount - floorNumber)))
-      this.$refs.elevator.style.top=`${(floorNumber-1)*100}px`
-      this.$refs.elevator.style.transitionDuration=`${Math.abs(this.curentFloor - (this.floorCount - floorNumber))}s`
-      this.curentFloor = this.floorCount - floorNumber
-    }
+    async btnHandler(floorNumber,click) {
+      if(this.curentFloor == (this.floorCount - floorNumber)){
+        return
+      }
+      if( this.isActive.includes(floorNumber) && click){
+        return
+      }
+      if (this.isBusy){
+        this.isActive.push(floorNumber)
+        return
+      }
+      this.isBusy = true
+      if (click) this.isActive.push(floorNumber)
+      let time = Math.abs(this.curentFloor - (this.floorCount - floorNumber))
+      this.$refs.elevator.style.top = `${(floorNumber - 1) * 100}px`;
+      this.$refs.elevator.style.transitionDuration = `${time}s`;
+      this.curentFloor = this.floorCount - floorNumber;
+      await new Promise(resolve => setTimeout(resolve, time*1000))
+      let index = this.isActive.indexOf(floorNumber)
+      console.log(this.isActive)
+      this.isActive.splice(index,1)
+      this.isBlink = true
+      await new Promise(resolve => setTimeout(resolve, 3000))
+      this.isBlink = false
+      this.isBusy = false
+    },
   },
-}
+};
 </script>
 
 <style>
-.main{
+.main {
   display: flex;
 }
-body{
+body {
   background: rgb(232, 232, 232);
 }
-*{
+* {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
 }
-.floors{
+.floors {
   margin-top: 5px;
   margin-left: 30px;
   width: 102px;
   border: 1px solid black;
 }
-.elevator{
+.elevator {
   transition: all linear;
   transition-duration: 1s;
   position: relative;
   background: coral;
   width: 100%;
   height: 99px;
-  top: 600px;
 }
-.grid{
+.grid {
   position: absolute;
   width: 100%;
   height: 1px;
   background: rgb(212, 212, 212);
 }
-.elevator-button{
-  position:absolute;
+.elevator-button {
+  border: 1px solid black;
+  position: absolute;
   width: 20px;
+  margin-left: 5px;
   height: 20px;
   border-radius: 100%;
+  cursor: pointer;
+
+}
+.active{
+  background: red;
+}
+.blink {
+  animation: .5s linear 0s infinite alternate blink-animation;
+  -webkit-animation: .5s linear 0s infinite alternate blink-animation;
+}
+@keyframes blink-animation {
+  from {
+    opacity: 100%;
+  }
+  to {
+    opacity: 20%;
+  }
+}
+@-webkit-keyframes blink-animation {
+  from {
+    opacity: 100%;
+  }
+  to {
+    opacity: 40%;
+  }
 }
 </style>
